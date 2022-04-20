94f13ef4d86229e4423c6020cbea3c96e48fea0b
leofrere <leofrere94@gmail.com>
2022-04-20 11:37:54 +0200

    update LogController

--- src/main/java/com/api/controller/LogController.java
+++ src/main/java/com/api/controller/LogController.java
@@ -1,19 +1,13 @@
 package com.api.controller;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.util.LinkedList;
 import java.util.List;
 
-import com.api.model.Compile;
+import com.api.model.JenkinsBuild;
 import com.api.model.Log;
-import com.api.model.Package;
-import com.api.model.Test;
-import com.api.repository.CompileRepository;
-import com.api.repository.LogRepository;
-import com.api.repository.PackageRepository;
-import com.api.repository.TestRepository;
+import com.api.services.JenkinsBuildService;
+import com.api.services.LogService;
+import com.api.utils.ReaderBuild;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,52 +16,38 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LogController {
-    
-    @Autowired
-    private CompileRepository compileRepository;
 
     @Autowired
-    private TestRepository testRepository;
+    private JenkinsBuildService jenkinsBuildService;
 
     @Autowired
-    private PackageRepository packageRepository;
-
-    @Autowired
-    private LogRepository logRepository;
+    private LogService logService;
 
     @GetMapping("/log/create")
-    public void setLog(@RequestParam(value="path") String path) {
-        File file = new File(path);
-        BufferedReader reader;
+    public void setLog(@RequestParam(value="pj") String projectName, @RequestParam(value="build") int buildNumber) {
+
+        JenkinsBuild jenkinsBuild = jenkinsBuildService.getJenkinsBuildByProjectName(projectName);
+        if (jenkinsBuild == null) {
+            return ;
+        }
 
         try {
-            reader = new BufferedReader(new FileReader(file));
-            Compile compile = compileRepository.save(new Compile(reader));
-            Test test = testRepository.save(new Test(reader));
-            Package package1 = packageRepository.save(new Package(reader));
-            logRepository.save(new Log( "mavenGL", compile, test, package1));
+            BufferedReader reader = new BufferedReader(ReaderBuild.readBuild(jenkinsBuild, buildNumber));
+            logService.addLog(reader, buildNumber);
         } catch (Exception e) {
             e.printStackTrace();
         }
+        
     }
 
     @GetMapping("/log")
     public Log getLog(@RequestParam(value="id") Long id) {
-        return logRepository.findById(id).get();
+        return logService.getLogById(id);
     }
 
     @GetMapping("/log/project")
     public List<Log> getLogByProject(@RequestParam(value="project") String project) {
-        List<Log> list = logRepository.findAll();
-        LinkedList<Log> logOfProject = new LinkedList<Log>();
-
-        for (Log log : list) {
-            if (log.getProject().equals(project)) {
-                logOfProject.add(log);
-            }
-        }
-
-        return logOfProject;
+        return logService.getLogByProject(project);
     }
 
 }
