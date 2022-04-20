b1f2ab5211aa1a63ab5ee5e33fac908d3a8c020e
leofrere <leofrere94@gmail.com>
2022-04-20 10:02:17 +0200

    add JenkinsBuild model

---
+++ src/main/java/com/api/model/JenkinsBuild.java
@@ -0,0 +1,76 @@
+package com.api.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "builds")
+public class JenkinsBuild {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    private String url;
+
+    @NotNull
+    private String projectName;
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String password;
+
+    public JenkinsBuild() {
+    }
+
+    public JenkinsBuild(String url, String projectName, String username, String password) {
+        this.url = url;
+        this.projectName = projectName;
+        this.username = username;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
