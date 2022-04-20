3313ed84ab5ae75f72dcedf52a068eb23fbd1d15
leofrere <leofrere94@gmail.com>
2022-04-20 13:46:00 +0200

    add method to get the last build number of a JenkinsBuild

--- src/main/java/com/api/utils/ReaderBuild.java
+++ src/main/java/com/api/utils/ReaderBuild.java
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Base64;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.api.model.JenkinsBuild;
 
@@ -24,9 +26,40 @@ public class ReaderBuild {
             InputStream content = connection.getInputStream();
             return new BufferedReader(new InputStreamReader(content));
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("URL: " + jenkinsBuild.getUrl() + "/job/" + jenkinsBuild.getProjectName() + "/" + String.valueOf(buildNumber) + "/consoleText not exist");
         }
         return null;
+    }
+
+    public static int lastBuild(JenkinsBuild jenkinsBuild) throws Exception {
+        try {
+            URL url = new URL(jenkinsBuild.getUrl() + "/job/" + jenkinsBuild.getProjectName() + "/lastBuild/");
+            String authStr = jenkinsBuild.getUsername() + ":" + jenkinsBuild.getPassword();
+            String encoding = Base64.getEncoder().encodeToString(authStr.getBytes("utf-8"));
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setDoOutput(true);
+            connection.setRequestProperty("Authorization", "Basic " + encoding);
+            InputStream content = connection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+            String line = null;
+            Pattern p1 = Pattern.compile("<title>(.*?)</title>");
+            while ((line = reader.readLine()) != null) {
+                Matcher m1 = p1.matcher(line);
+                if (m1.find()){
+                    Pattern p2 = Pattern.compile("#[0-9]*");
+                    Matcher m2 = p2.matcher(m1.group(0));
+                    if (m2.find()) {
+                        return Integer.parseInt(m2.group(0).replace("#", ""));
+                    }
+                }
+                
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return -1;
     }
 
 }
