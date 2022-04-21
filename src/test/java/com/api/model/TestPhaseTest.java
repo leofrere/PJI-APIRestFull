7f41fc3070885566810a88e68426e25ceba2e751
leofrere <leofrere94@gmail.com>
2022-04-21 11:22:46 +0200

    add test with error in test phase

--- src/test/java/com/api/model/TestPhaseTest.java
+++ src/test/java/com/api/model/TestPhaseTest.java
@@ -35,4 +35,23 @@ public class TestPhaseTest {
         }
     }
 
+    @Test
+    void creationOfTestPhaseWithErrorInLogTest() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(getClass().getResource("logfileWithTestError.log").getFile()));
+            new CompilePhase(reader);
+            TestPhase test = new TestPhase(reader);
+            assertEquals("ko", test.getStatus());
+            assertEquals(3, test.getNumberOfTestClasses());
+            assertEquals("09:10:12,231 [ERROR] /Users/leofrere/.jenkins/workspace/mavenGL/src/test/java/bank/CompteTest.java:[151,31] cannot find symbol", test.getErrorsTrace());
+            assertEquals(null, test.getTime());
+            assertEquals(0, test.getTestsRun());
+            assertEquals(0, test.getTestsFailed());
+            assertEquals(0, test.getTestsError());
+            assertEquals(0, test.getTestsSkipped());
+        } catch (FileNotFoundException e) {
+            assertTrue(false);
+        }
+    }
+
 }
