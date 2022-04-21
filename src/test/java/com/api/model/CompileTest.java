cb23170da12c7c3dd1ce0eb332b23d7c4cdda277
leofrere <leofrere94@gmail.com>
2022-04-21 10:55:38 +0200

    add test for compile with no error

--- src/test/java/com/api/model/CompileTest.java
+++ src/test/java/com/api/model/CompileTest.java
@@ -1,5 +1,12 @@
 package com.api.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 import com.api.PJIApplication;
 
 import org.junit.jupiter.api.Test;
@@ -10,7 +17,16 @@ public class CompileTest {
     
     @Test
     void creationOfCompileTest() {
-        
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(getClass().getResource("logfile.log").getFile()));
+            Compile compile = new Compile(reader);
+            assertEquals("ok", compile.getStatus());
+            assertEquals(14, compile.getNumberOfClasses());
+            assertEquals("", compile.getErrorsTrace());
+            assertEquals("2,46s", compile.getTime());
+        } catch (FileNotFoundException e) {
+            assertTrue(false);
+        }
     }
 
 }
