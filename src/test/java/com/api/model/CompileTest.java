24ef33e07756995b8f7a1cdb2dcdbf578f6d538a
leofrere <leofrere94@gmail.com>
2022-04-21 11:30:26 +0200

    add test with error in compile phase

--- src/test/java/com/api/model/CompileTest.java
+++ src/test/java/com/api/model/CompileTest.java
@@ -29,4 +29,18 @@ public class CompileTest {
         }
     }
 
+    @Test
+    void creationOfCompileWithErrorInLogTest() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(getClass().getResource("logfileWithCompileError.log").getFile()));
+            CompilePhase compile = new CompilePhase(reader);
+            assertEquals("ko", compile.getStatus());
+            assertEquals(14, compile.getNumberOfClasses());
+            assertEquals("11:26:35,105 [ERROR] /Users/leofrere/.jenkins/workspace/mavenGL/src/main/java/bank/CompteEpargne.java:[11,17] cannot find symbol", compile.getErrorsTrace());
+            assertEquals(null, compile.getTime());
+        } catch (FileNotFoundException e) {
+            assertTrue(false);
+        }
+    }
+
 }
