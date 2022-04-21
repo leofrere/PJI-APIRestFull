0c3c186dd60ed7007b528b749a0bc57af875e7b9
leofrere <leofrere94@gmail.com>
2022-04-21 11:42:55 +0200

    revert last commit

--- src/main/java/com/api/model/JenkinsBuild.java
+++ src/main/java/com/api/model/JenkinsBuild.java
@@ -66,6 +66,10 @@ public class JenkinsBuild {
         this.username = username;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
     public void setPassword(String password) {
         this.password = password;
     }
