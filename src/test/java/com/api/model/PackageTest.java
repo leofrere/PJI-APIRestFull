f6b3de90f34e806b6d86ceb24a08478e5015346d
leofrere <leofrere94@gmail.com>
2022-04-21 11:10:30 +0200

    add test for the creation of PackagePhase

--- src/test/java/com/api/model/PackageTest.java
+++ src/test/java/com/api/model/PackageTest.java
@@ -1,5 +1,12 @@
 package com.api.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 import com.api.PJIApplication;
 
 import org.junit.jupiter.api.Test;
@@ -8,4 +15,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(classes = PJIApplication.class)
 public class PackageTest {
     
+    @Test
+    void creationOfPackageTest() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(getClass().getResource("logfile.log").getFile()));
+            new CompilePhase(reader);
+            new TestPhase(reader);
+            PackagePhase packagePhase = new PackagePhase(reader);
+            assertEquals("ok", packagePhase.getStatus());
+            assertEquals("/Users/leofrere/.jenkins/workspace/mavenGL/target/BankMaven-0.0.1-SNAPSHOT.jar", packagePhase.getJarPath());
+            assertEquals("", packagePhase.getErrorsTrace());
+            assertEquals("0,04s", packagePhase.getTime());
+        } catch (FileNotFoundException e) {
+            assertTrue(false);
+        }
+    }
+
 }
