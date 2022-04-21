0b8a9eafa1f8b283537716090c9ae8ec73f1069e
leofrere <leofrere94@gmail.com>
2022-04-21 11:05:35 +0200

    add test creation of TestPhase + rename

--- src/test/java/com/api/model/TestPhaseTest.java
+++ src/test/java/com/api/model/TestPhaseTest.java
@@ -1,5 +1,7 @@
 package com.api.model;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -9,21 +11,25 @@ import java.io.FileReader;
 
 import com.api.PJIApplication;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = PJIApplication.class)
-public class CompileTest {
+public class TestPhaseTest {
     
     @Test
-    void creationOfCompileTest() {
+    void creationOfTestTest() {
         try {
             BufferedReader reader = new BufferedReader(new FileReader(getClass().getResource("logfile.log").getFile()));
             CompilePhase compile = new CompilePhase(reader);
-            assertEquals("ok", compile.getStatus());
-            assertEquals(14, compile.getNumberOfClasses());
-            assertEquals("", compile.getErrorsTrace());
+            TestPhase test = new TestPhase(reader);
+            assertEquals("ok", test.getStatus());
+            assertEquals(3, test.getNumberOfTestClasses());
+            assertEquals("", test.getErrorsTrace());
             assertEquals("2,46s", compile.getTime());
+            assertEquals(26, test.getTestsRun());
+            assertEquals(0, test.getTestsFailed());
+            assertEquals(0, test.getTestsError());
+            assertEquals(0, test.getTestsSkipped());
         } catch (FileNotFoundException e) {
             assertTrue(false);
         }
