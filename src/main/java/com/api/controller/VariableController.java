f1d582818f47cc9213ea7cadd076d370bfb19268
leofrere <leofrere94@gmail.com>
2022-05-02 14:11:08 +0200

    add the choice of module for evolution of variable

--- src/main/java/com/api/controller/VariableController.java
+++ src/main/java/com/api/controller/VariableController.java
@@ -10,20 +10,21 @@ import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/evolution")
 public class VariableController {
 
-    @GetMapping("/phase/{projectName}/{phase}/{variable}")
-    public Map<Integer, String> evolutionOfPhaseVariable(@PathVariable String projectName, @PathVariable String phaseName, @PathVariable String variable) throws Exception{
+    @GetMapping("/phase/{projectName}/{phaseName}/{variable}")
+    public Map<Integer, String> evolutionOfPhaseVariable(@PathVariable String projectName, @PathVariable String phaseName, @PathVariable String variable, @RequestParam(defaultValue = "0") int module) throws Exception{
         Map<Integer, String> map = new HashMap<Integer, String>();
-        JSONObject data = new JSONObject(GraphQLRequest.evolutionOfPhaseVariable(projectName, phaseName, variable));
+        JSONObject data = new JSONObject(GraphQLRequest.evolutionOfPhaseVariable(projectName, phaseName+"Phase", variable));
         JSONArray tab = getLogs(data);
         for(int i = 0; i < tab.length(); i++){
             int build = tab.getJSONObject(i).getInt("build");
-            JSONObject phase = getPhase(phaseName, tab, i);
+            JSONObject phase = getPhase(phaseName+"Phase", tab, i, module);
             String value = phase.get(variable).toString();
             map.put(build, value);
         }
@@ -31,13 +32,13 @@ public class VariableController {
     }
 
     @GetMapping("/test/{projectName}/{classeName}/{variable}")
-    public Map<Integer, String> evolutionOfTestClasseVariable(@PathVariable String projectName, @PathVariable String classeName, @PathVariable String variable) throws Exception{
+    public Map<Integer, String> evolutionOfTestClasseVariable(@PathVariable String projectName, @PathVariable String classeName, @PathVariable String variable, @RequestParam(defaultValue = "0") int module) throws Exception{
         Map<Integer, String> map = new HashMap<Integer, String>();
         JSONObject data = new JSONObject(GraphQLRequest.evolutionOfTestClasseVariable(projectName, variable));
         JSONArray tab = getLogs(data);
         for(int i = 0; i < tab.length(); i++){
             int build = tab.getJSONObject(i).getInt("build");
-            JSONObject phase = getPhase("testPhase", tab, i);
+            JSONObject phase = getPhase("testPhase", tab, i, module);
             JSONArray tests = phase.getJSONArray("testsByClasse");
             for(int j = 0; j < tests.length(); j++){
                 JSONObject test = tests.getJSONObject(j);
@@ -58,9 +59,9 @@ public class VariableController {
         return tab;
     }
 
-    private JSONObject getPhase(String phaseName, JSONArray tab, int i) {
+    private JSONObject getPhase(String phaseName, JSONArray tab, int i, int module) {
         JSONArray orders = tab.getJSONObject(i).getJSONArray("orders");
-        JSONObject phase = orders.getJSONObject(0).getJSONObject(phaseName);
+        JSONObject phase = orders.getJSONObject(module).getJSONObject(phaseName);
         return phase;
     }
     
