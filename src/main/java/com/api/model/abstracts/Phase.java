d4eceabd0c5e02f3eb7b8f7543e4fba50606e3c0
leofrere <leofrere94@gmail.com>
2022-05-03 16:32:33 +0200

    add Time abstract class

--- src/main/java/com/api/model/abstracts/Phase.java
+++ src/main/java/com/api/model/abstracts/Phase.java
@@ -3,21 +3,11 @@ package com.api.model.abstracts;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
-public abstract class Phase extends Model {
-    
-    protected String time;
+public abstract class Phase extends Time {
 
     protected String status;
 
     protected String errorsTrace = "";
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
 
     public String getStatus() {
         return status;
@@ -33,13 +23,6 @@ public abstract class Phase extends Model {
 
     public void setErrorsTrace(String errorsTrace) {
         this.errorsTrace = errorsTrace;
-    }
-
-    public float getTimeFloat() {
-        if(time == null){
-            return 0;
-        }
-        return Float.parseFloat(time.replace(",", ".").replace("s", ""));
     }
 
 }
---
+++ src/main/java/com/api/model/abstracts/Time.java
@@ -0,0 +1,22 @@
+package com.api.model.abstracts;
+
+public abstract class Time extends Model {
+    
+    protected String time;
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public float getTimeFloat() {
+        if(time == null){
+            return 0;
+        }
+        return Float.parseFloat(time.replace(",", ".").replace("s", ""));
+    }
+
+}
