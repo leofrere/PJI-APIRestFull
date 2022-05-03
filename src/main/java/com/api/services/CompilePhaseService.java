5647994e6821e31dc8af8165845c701d3889fa4a
leofrere <leofrere94@gmail.com>
2022-05-03 10:03:47 +0200

    add some query graphql for CompilePhase

--- src/main/java/com/api/services/CompilePhaseService.java
+++ src/main/java/com/api/services/CompilePhaseService.java
@@ -1,6 +1,8 @@
 package com.api.services;
 
 import java.io.BufferedReader;
+import java.util.LinkedList;
+import java.util.List;
 
 import com.api.model.CompilePhase;
 import com.api.repository.CompileRepository;
@@ -8,7 +10,12 @@ import com.api.repository.CompileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import io.leangen.graphql.annotations.GraphQLArgument;
+import io.leangen.graphql.annotations.GraphQLQuery;
+import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
+
 @Service
+@GraphQLApi
 public class CompilePhaseService {
     
     @Autowired
@@ -27,6 +34,66 @@ public class CompilePhaseService {
 
     public void deleteCompilePhase(long id) {
         compileRepository.deleteById(id);
+    }
+
+    @GraphQLQuery(name = "compilePhases")
+    public List<CompilePhase> getAllCompilePhases() {
+        return compileRepository.findAll();
+    }
+
+    @GraphQLQuery(name = "compilePhaseByProject")
+    public List<CompilePhase> getCompilePhaseByProject(@GraphQLArgument(name = "project") String projectName) {
+        List<CompilePhase> compilePhases = compileRepository.findAll();
+        List<CompilePhase> list = new LinkedList<CompilePhase>();
+        for (CompilePhase compilePhase : compilePhases) {
+            if (compilePhase.getProject().equals(projectName)) {
+                list.add(compilePhase);
+            }
+        }
+        return list;
+    }
+
+    @GraphQLQuery(name = "compilePhaseByTime")
+    public List<CompilePhase> getCompilePhaseByTime(@GraphQLArgument(name = "time") String time, @GraphQLArgument(name = "op") String op, @GraphQLArgument(name = "project") String projectName) {
+        List<CompilePhase> compilePhases = compileRepository.findAll();
+        List<CompilePhase> list = new LinkedList<CompilePhase>();
+        for (CompilePhase compilePhase : compilePhases) {
+            if (compilePhase.getProject().equals(projectName)){
+                cmpCompile(Float.parseFloat(time), compilePhase.getTimeFloat(), op, list, compilePhase);
+            }
+        }
+        return list;
+    }
+
+    @GraphQLQuery(name = "compilePhaseByCompiled")
+    public List<CompilePhase> getCompilePhaseByCompiled(@GraphQLArgument(name = "compiled") int compiled, @GraphQLArgument(name = "op") String op, @GraphQLArgument(name = "project") String projectName) {
+        List<CompilePhase> compilePhases = compileRepository.findAll();
+        List<CompilePhase> list = new LinkedList<CompilePhase>();
+        for (CompilePhase compilePhase : compilePhases) {
+            if (compilePhase.getProject().equals(projectName)){
+                cmpCompile((float) compiled,(float) compilePhase.getCompiledClasses(), op, list, compilePhase);
+            }
+        }
+        return list;
+    }
+
+    private void cmpCompile(Float a, Float b, String op, List<CompilePhase> compilePhases, CompilePhase compilePhase) {
+        switch(op) {
+            case ">":
+                if (a > b) {
+                    compilePhases.add(compilePhase);
+                }
+                break;
+            case "<":
+                if (a < b) {
+                    compilePhases.add(compilePhase);
+                }
+                break;
+            case "=":
+                if (a == b) {
+                    compilePhases.add(compilePhase);
+                }
+        }
     }
 
 }
