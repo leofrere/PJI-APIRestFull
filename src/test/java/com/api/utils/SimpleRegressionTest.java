5cdfc807ac854d13394c5c281152d94b9c84d657
leofrere <leofrere94@gmail.com>
2022-05-06 10:58:22 +0200

    add tests for regression

--- src/test/java/com/api/utils/SimpleRegressionTest.java
+++ src/test/java/com/api/utils/SimpleRegressionTest.java
@@ -2,6 +2,8 @@ package com.api.utils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Map;
+
 import org.json.JSONArray;
 import org.junit.jupiter.api.Test;
 
@@ -19,6 +21,32 @@ public class SimpleRegressionTest {
         JSONArray tab = new JSONArray("[{\"build\":1,\"value\":7.6},{\"build\":2,\"value\":4.1},{\"build\":3,\"value\":3.39},{\"build\":4,\"value\":2.86},{\"build\":5,\"value\":1.7},{\"build\":6,\"value\":13.13}]");
         float res = SimpleRegression.minFloat(tab, "value");
         assertEquals(1.7, res, 0.01);
+    }
+
+    @Test
+    public void regressionIntTest(){
+        JSONArray tab = new JSONArray("[{\"build\":1,\"value\":7},{\"build\":2,\"value\":4},{\"build\":3,\"value\":3},{\"build\":4,\"value\":2},{\"build\":5,\"value\":1},{\"build\":6,\"value\":13}]");
+        Map<String,Float>[] res = SimpleRegression.regressionInt(tab, "value");
+        assertEquals(6, res.length);
+        assertEquals(2.73, res[0].get("value").floatValue(), 0.01);
+        assertEquals(4.47, res[1].get("value").floatValue(), 0.01);
+        assertEquals(6.20, res[2].get("value").floatValue(), 0.01);
+        assertEquals(7.94, res[3].get("value").floatValue(), 0.01);
+        assertEquals(9.68, res[4].get("value").floatValue(), 0.01);
+        assertEquals(11.41, res[5].get("value").floatValue(), 0.01);
+    }
+
+    @Test
+    public void regressionFloatTest(){
+        JSONArray tab = new JSONArray("[{\"build\":1,\"value\":7.6},{\"build\":2,\"value\":4.1},{\"build\":3,\"value\":3.39},{\"build\":4,\"value\":2.86},{\"build\":5,\"value\":1.7},{\"build\":6,\"value\":13.13}]");
+        Map<String,Float>[] res = SimpleRegression.regressionFloat(tab, "value");
+        assertEquals(6, res.length);
+        assertEquals(3.34, res[0].get("value").floatValue(), 0.01);
+        assertEquals(4.98, res[1].get("value").floatValue(), 0.01);
+        assertEquals(6.62, res[2].get("value").floatValue(), 0.01);
+        assertEquals(8.27, res[3].get("value").floatValue(), 0.01);
+        assertEquals(9.91, res[4].get("value").floatValue(), 0.01);
+        assertEquals(11.55, res[5].get("value").floatValue(), 0.01);
     }
 
 }
